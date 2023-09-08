e0869426e910459ccb1e1ab24fcfbc8391741a19
AgPrieto <aguspv99@gmail.com>
2023-09-08 17:37:17 -0300

    updateeeee

---
+++ array.js
@@ -0,0 +1,74 @@
+//var listaDeCompras = [];
+//listaDeCompras[3] = "Tomates";
+//listaDeCompras[1] = "Lechuga";
+//listaDeCompras[2] = "Zanahoria";
+
+//var elementoArray = listaDeCompras[1];
+//console.log(elementoArray);
+
+//console.log(listaDeCompras.length);
+
+//var colores = ["amarillo", "azul"];
+//colores.push("rojo");
+//colores.unshift("verde");
+//colores.pop();
+//colores.shift();
+//console.log(colores);
+
+//var pintores = ["Picasso", "Velazquez", "Van gogh", "Dali"];
+//var incluyePicasso = pintores.includes("Picasso");
+//console.log(incluyePicasso);
+
+//var numeros = [1, 2, 3, 4 ,5];
+//var cumplenCondicion = numeros.every((num) => {
+  //  return num <= 5;
+// })
+//console.log(cumplenCondicion);
+
+//var palabra = "Henri";
+//var palabraSplit = palabra.split("");
+
+//palabraSplit.pop();
+//palabraSplit.push("y");
+
+//var palabraFix = palabraSplit.join("");
+
+//console.log(palabraFix);
+
+//var numeros = [1, 2, 3, 4]; 
+//numeros.forEach((num) => console.log(num));
+//numeros.forEach(num => {
+  //  if(num === 3) {
+    //console.log(num)
+//}
+//});
+
+//var masUno = numeros.map(num =>{
+//return num + 1;})
+  //    console.log(masUno);
+
+  //var arr = [1, 2, 3, 4, 5];
+//for(var i = 0; i < arr.length; i++) {
+  //  console.log(arr[i]);
+//}
+
+//function encontrarLetraP (string){
+ //var arr = string.split("");
+ //for(let i = 0; i < arr.length; i++) {
+   // if(arr[i] === "p" || arr[i] === "P"){
+     //   console.log("Si tiene letra p");
+    //} 
+    
+ //}
+ 
+//}
+
+//encontrarLetraP ("Prieto");
+
+
+var arr = []
+do {
+arr.push("boom");
+}while (arr.length < 5);
+
+console.log(arr);
